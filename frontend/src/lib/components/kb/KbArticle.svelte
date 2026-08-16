f101951e9b3d425eb9d885cb7f9bf6d82db86797
Lowder <me@lowderplay.dev>
2026-08-17 02:04:01 +0500

    docs: update probing info

--- frontend/src/lib/components/kb/KbArticle.svelte
+++ frontend/src/lib/components/kb/KbArticle.svelte
@@ -14,6 +14,7 @@ kb?.reset();
 		[&_code]:bg-neutral-900/70 [&_code]:px-1.5 [&_code]:py-0.5 [&_code]:rounded [&_code]:text-sm [&_code]:text-neutral-200
 		[&_h1]:mb-16 [&_h1]:text-5xl [&_h1]:font-black [&_h1]:tracking-tight [&_h1]:text-neutral-100 [&_h1]:uppercase
 		[&_li]:my-3 [&_li]:text-neutral-300
+		[&_ol]:list-decimal [&_ol]:pl-6 [&_ol]:my-6
 		[&_p]:my-6 [&_p]:text-lg [&_p]:leading-relaxed [&_p]:text-neutral-300
 		[&_ul]:list-disc [&_ul]:pl-6 [&_ul]:my-6
 	"
